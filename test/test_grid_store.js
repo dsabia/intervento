e06ddb7bc64a19edffa55646f29615b16bb8cfbc
dsabia <dsabia@hotmail.it>
2016-10-14 09:15:30 +0100

    Update test_grid_store.js

--- test/test_grid_store.js
+++ test/test_grid_store.js
@@ -40,41 +40,4 @@ describe('GridStore', function() {
       });
     });
   });
-
-  it('saveAndRead() should read the file saved previously', function() {
-    var file_path = '/resources/aliensmokepipe.jpg';
-    var path = __dirname + file_path;
-    var userPath = 'user/id';
-
-    var mongo_file_name = userPath+file_path;
-    var fileId = new ObjectID();
-    // Our file ID
-//    console.log(db);
-
-    // Open a new file
-    var gridStore = new GridStore(db, fileId, 'w');
-//    console.log(gridStore);
-    // Read the filesize of file on disk (provide your own)
-    var fileSize = fs.statSync(path).size;
-    // Read the buffered data for comparision reasons
-    var data = fs.readFileSync(path);
-
-    gridStore.writeFile(path, mongo_file_name, 'w', function(err, doc) {
-      console.log('write ' - err);
-      //db.close();
-    });
-
-    gridStore.seek(0);
-
-    // Read back all the written content and verify the correctness
-    gridStore.read(db, mongo_file_name, 'r', function(err, fileData) {
-      console.log('read ' - err);
-      assert.equal(data.toString('base64'), fileData.toString('base64'))
-      console.log(fileData);
-      //assert.equal(fileSize, fileData.length);
-
-      //db.close();
-    });
-
-  });
 });
