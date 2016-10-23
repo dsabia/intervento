d451be1d92f7ef4f471981bb80c042113fb9516a
Daniele <dsabia@hotmail.it>
2016-10-23 23:03:14 +0100

    Table update and some fix

--- public/javascripts/intervento/add.js
+++ public/javascripts/intervento/add.js
@@ -1,10 +1,10 @@
 var inputOraInizio = $('#id-ora_inizio');
 inputOraInizio.clockpicker({
-    autoclose: true,
+    'autoclose': true,
     'default': 'now'
 });
 var inputOraFine = $('#id-ora_fine');
 inputOraFine.clockpicker({
-    autoclose: true,
+    'autoclose': true,
     'default': 'now'
 });
