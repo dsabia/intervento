35b98dd03d91130cc2a7f0d2e3cf96548c9cff55
Daniele <dsabia@hotmail.it>
2016-11-03 22:18:57 +0000

    Clienti edit plus some minor fix

--- routes/clienti.js
+++ routes/clienti.js
@@ -19,6 +19,13 @@ router.get('/add', ensureAuthenticated, function(req, res, next) {
   res.render('app/clienti/scheda', { title: 'Aggiungi un cliente'});
 });
 
+// open page edit cliente
+router.get('/edit/:codice', ensureAuthenticated, function(req, res, next) {
+  Customer.findOne({ 'codice' :  req.params.codice }, function(err, customer) {
+    res.render('app/clienti/scheda', { title: 'Modifica cliente', customer: customer});
+  });
+});
+
 // load detail page by code
 router.get('/:codice', ensureAuthenticated, function(req, res, next) {
   Customer.findOne({ 'codice' :  req.params.codice }, function(err, customer) {
@@ -32,25 +39,34 @@ router.get('/:codice', ensureAuthenticated, function(req, res, next) {
 
 // add form data on db
 router.post('/add', ensureAuthenticated, function(req, res, next) {
-  var customer = new Customer();
-  console.log(req.body.codice);
-  console.log(customer);
-  customer.codice                = req.body.codice;
-  customer.sconto                = req.body.sconto;
-  customer.ragione_sociale       = req.body.ragione_sociale;
-  customer.piva_cf               = req.body.piva_cf;
-  customer.indirizzo             = req.body.indirizzo;
-  customer.telefono              = req.body.telefono;
-  customer.email                 = req.body.email;
-  customer.save(function(err) {
-      console.log('save ' + err);
-      if (err)
-          throw err;
-      return;
-  });
-
-  res.redirect('/clienti/'+customer.codice);
+  if(req.body.id){
+    Customer.findById(req.body.id, function(err, pojo) {
+      populateRequestAndSave(req, pojo);
+      res.redirect('/clienti/'+pojo.codice);
+    });
+  }else{
+    var customer = new Customer();
+    populateRequestAndSave(req, customer);
+    res.redirect('/clienti/'+customer.codice);
+  }
 });
+
+function populateRequestAndSave(req, customer){
+    customer.codice                = req.body.codice;
+    customer.sconto                = req.body.sconto;
+    customer.ragione_sociale       = req.body.ragione_sociale;
+    customer.piva_cf               = req.body.piva_cf;
+    customer.indirizzo             = req.body.indirizzo;
+    customer.telefono              = req.body.telefono;
+    customer.email                 = req.body.email;
+
+    customer.save(function(err) {
+        console.log('save ' + err);
+        if (err)
+            throw err;
+        return;
+    });
+}
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
