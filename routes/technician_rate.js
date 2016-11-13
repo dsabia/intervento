adb76070c833ad178c8becab1ee124bfea3d39ac
Daniele <dsabia@hotmail.it>
2016-11-13 18:17:25 +0000

    Technician rate work

--- routes/technician_rate.js
+++ routes/technician_rate.js
@@ -14,22 +14,22 @@ router.get('/', appUtil.ensureAuthenticated, function(req, res, next) {
     }
     if(list_results.length > 0){
       var pojo = list_results[0];
-      res.render('app/tariffe/detail', { title: 'Dettaglio della tariffa', rate: pojo });
+      res.render('app/technician_rate/detail', { title: 'Dettaglio della tariffa', rate: pojo });
     }else{
-      res.render('app/tariffe/detail', { title: 'Tariffa non presente'});
+      res.render('app/technician_rate/detail', { title: 'Tariffa non presente'});
     }
   });
 });
 
 /* open page add new tariffa */
 router.get('/add', appUtil.ensureAuthenticated, function(req, res, next) {
-  res.render('app/tariffe/edit', { title: 'Aggiungi tariffa', frazioni_dora_option: frazioni_dora_option });
+  res.render('app/technician_rate/edit', { title: 'Aggiungi tariffa', frazioni_dora_option: frazioni_dora_option });
 });
 
 /* open page add new tariffa */
 router.get('/edit', appUtil.ensureAuthenticated, function(req, res, next) {
   TechnicianRate.findOne({ 'owner' : req.user._id }, function(err, pojo){
-    res.render('app/tariffe/edit', { title: 'Modifica tariffa',
+    res.render('app/technician_rate/edit', { title: 'Modifica tariffa',
                                      rate: pojo,
                                      frazioni_dora_option: appUtil.setSelectedOption(frazioni_dora_option, pojo.frazioni_ora) });
   });
@@ -40,21 +40,21 @@ router.post('/edit', appUtil.ensureAuthenticated, function(req, res, next) {
   if(req.body.id){
     TechnicianRate.findById(req.body.id, function(err, rate){
       populateRequestAndSave(req, rate);
-      res.redirect('/tariffe/');
+      res.redirect('/technician_rate/');
     });
   }else{
     var rate = new TechnicianRate();
     populateRequestAndSave(req, rate);
-    res.redirect('/tariffe/');
+    res.redirect('/technician_rate/');
   }
 });
 
 function populateRequestAndSave(req, rate){
-  rate.diritto_chiamata   = req.body.diritto_chiamata;
-  rate.tariffa_fissa      = req.body.tariffa_fissa;
-  rate.tariffa_km         = req.body.tariffa_km;
-  rate.tariffa_oraria     = req.body.tariffa_oraria;
-  rate.frazioni_ora       = req.body.frazioni_ora;
+  rate.consultancy_fee    = req.body.consultancy_fee;
+  rate.fixed_rate         = req.body.fixed_rate;
+  rate.km_rate            = req.body.km_rate;
+  rate.hour_rate          = req.body.hour_rate;
+  rate.fraction_of_hour   = req.body.fraction_of_hour;
   rate.owner              = req.user._id;
 
   rate.save(function(err) {
