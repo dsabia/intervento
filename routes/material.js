beb52a68547047ca97c7b50db6b9c318436b028a
Daniele <dsabia@hotmail.it>
2016-11-13 17:59:58 +0000

    Material work

--- routes/material.js
+++ routes/material.js
@@ -10,37 +10,37 @@ router.get('/', appUtil.ensureAuthenticated, function(req, res, next) {
       console.log(err);
       return;
     }
-    res.render('app/materiale/view', { title: 'Elenco materiali', list:  list_pojos });
+    res.render('app/material/view', { title: 'Elenco materiali', list:  list_pojos });
   });
 });
 
 /* open page add new material */
 router.get('/add', appUtil.ensureAuthenticated, function(req, res, next) {
-  res.render('app/materiale/add', { title: 'Aggiungi materiale'});
+  res.render('app/material/add', { title: 'Aggiungi materiale'});
 });
 
 /* open page edit material */
-router.get('/edit/:codice', appUtil.ensureAuthenticated, function(req, res, next) {
-  Material.findOne({ 'codice' :  req.params.codice, 'owner' : req.user._id}, function(err, pojo){
-    res.render('app/materiale/add', { title: 'Modifica materiale', material : pojo});
+router.get('/edit/:code', appUtil.ensureAuthenticated, function(req, res, next) {
+  Material.findOne({ 'code' :  req.params.code, 'owner' : req.user._id}, function(err, pojo){
+    res.render('app/material/add', { title: 'Modifica materiale', material : pojo});
   });
 });
 
 /* open page delete material */
 router.get('/delete/:id', appUtil.ensureAuthenticated, function(req, res, next) {
   Material.remove({ '_id' :  req.params.id }, function(err){
-    res.redirect('/materiale');
+    res.redirect('/material');
   });
 });
 
 // load detail page by code
-router.get('/:codice', appUtil.ensureAuthenticated, function(req, res, next) {
-  Material.findOne({ 'codice' :  req.params.codice, 'owner' : req.user._id}, function(err, pojo) {
+router.get('/:code', appUtil.ensureAuthenticated, function(req, res, next) {
+  Material.findOne({ 'code' :  req.params.code, 'owner' : req.user._id}, function(err, pojo) {
     if (err){
       pojo.log(err);
       return;
     }
-    res.render('app/materiale/view', { 'materiale': pojo});
+    res.render('app/material/view', { 'material': pojo});
   });
 });
 
@@ -49,25 +49,25 @@ router.post('/add', appUtil.ensureAuthenticated, function(req, res, next) {
   if(req.body.id){
     Material.findById(req.body.id, function(err, pojo){
       populateRequestAndSave(req, pojo);
-      res.redirect('/materiale/'+pojo.codice);
+      res.redirect('/material/'+pojo.codice);
     });
   }else{
     var material = new Material();
     populateRequestAndSave(req, material);
-    res.redirect('/materiale/'+material.codice);
+    res.redirect('/material/'+material.codice);
   }
 });
 
 function populateRequestAndSave(req, material){
-  material.codice    		   = req.body.codice;
-  material.nome_prodotto   = req.body.nome_prodotto;
-  material.descrizione     = req.body.descrizione;
-  material.prezzo     		 = req.body.prezzo;
+  material.code    		   = req.body.code;
+  material.product_name  = req.body.product_name;
+  material.description   = req.body.description;
+  material.price     		 = req.body.price;
   //material.matricola     	 = req.body.matricola;
   //material.sconto     		 = req.body.sconto;
   //material.quantita     	 = req.body.quantita;
   material.owner           = req.user._id;
-  
+
   material.save(function(err) {
       console.log('save ' + err);
       if (err)
