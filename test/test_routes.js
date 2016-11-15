3363e79e6c6fdfb23fbda71dbe512822a314de22
Daniele <dsabia@hotmail.it>
2016-11-15 21:59:19 +0000

    test REST api with express, close #6

---
+++ test/test_routes.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV = 'test';
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Index', function() {
+  it('should list index if not authenticate GET', function(done) {
+    chai.request(server)
+      .get('/')
+      .end(function(err, res){
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+});
